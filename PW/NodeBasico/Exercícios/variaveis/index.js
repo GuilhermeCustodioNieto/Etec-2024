37817c11e7425a28d65cb000b5f71c653465f508
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-02-27 06:43:22 -0300

    Exercicio base de NodeJS
    A base dos exercícios de node está sendo enviada, mas incompleta até o momento

---
+++ PW/NodeBasico/Exercícios/variaveis/index.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+//Faça um programa para ler dois valores inteiros A + B e imprima o resultado da expressão (A + B)
+inquirer
+  .prompt([
+    {
+      message: `Enter the A value: `,
+      name: "a",
+    },
+    {
+      message: "Enter the B value: ",
+      name: "b",
+    },
+  ])
+  .then((answers) => {
+    let a = Number(answers.a);
+    let b = Number(answers.b);
+    console.log(`Result of A + B: ${a + b}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
