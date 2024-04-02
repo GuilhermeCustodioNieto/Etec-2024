aeb40999233a2b2581578dd39ce85e149a9523d9
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-02 06:46:23 -0300

    BD - added someone's INSETS
    The tblVenda and tblProduto INSERTS has been completed

--- BD/Database Base/insert-producs.sql
+++ BD/Database Base/insert-producs.sql
@@ -21,3 +21,27 @@ VALUES
 ('Assai', 'Maria Stella'),
 ('Roldão', 'Paulo César')
 
+INSERT INTO tblProduto(descricaoProduto, valorProduto, quantidadeProduto, codFabricante, codFornecedor)
+VALUES 
+('Amaciante Downy', 5.76, 1500, 2,1),
+('Amaciante Confort', 4.45, 2300,1,1),
+('Sabão em pó OMO', 5.99, 1500, 1, 2),
+('Margaina Qualy', 4.76, 2500, 3,1),
+('Salsicha Hot Dog Sadia', 6.78, 2900, 3, 2),
+('Mortadela Perdigão', 2.50, 1200, 3, 3),
+('Hamburguer Sadia', 9.89, 1600, 3,1),
+('Fralda Pampers', 36.00, 340, 2, 3),
+('Xampu Seda', 5.89, 800, 1,2),
+('Condicionador Seda', 6.50, 700, 1, 3)
+
+INSERT INTO tblVenda(dataVenda, valorTotalVenda, codCliente)
+VALUES 
+('01-02-2014', 4500.00, 1),
+('03-02-2014', 3400, 1),
+('10-02-2014', 2100, 2),
+('15-02-2014', 2700, 3),
+('17-03-2014', 560, 3),
+('09-04-2014', 1200, 4),
+('07-05-2014', 3500, 5),
+('07-05-2014', 3400, 1),
+('05-05-2014', 4000, 2)
