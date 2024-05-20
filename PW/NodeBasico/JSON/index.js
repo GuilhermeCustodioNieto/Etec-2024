63ee426296b3332f2bc1a35d995d858b0901d637
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-05-20 20:30:57 -0300

    PW - json updated

--- PW/NodeBasico/JSON/index.js
+++ PW/NodeBasico/JSON/index.js
@@ -2,9 +2,4 @@ const fs = require("fs");
 
 let json = fs.readFileSync('./servicos.txt');
 json = JSON.parse(json);
-
-// json.forEach(object => {
-//     console.log(object);
-// });
-
 console.log(json);
