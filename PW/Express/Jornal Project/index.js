54ff7c62448a06b4a038525f5b08373a4ca77045
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-06-03 22:12:19 -0300

    PW - Noticia 5 feita

--- PW/Express/Jornal Project/index.js
+++ PW/Express/Jornal Project/index.js
@@ -1,37 +1,41 @@
-import express from 'express'
+import express from "express";
 
-import path from 'path'
+import path from "path";
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get(`/`, (req, res) => {
-    let index = path.resolve('./view/index.html')
-    res.sendFile(index)
-})
+  let index = path.resolve("./view/index.html");
+  res.sendFile(index);
+});
 
 app.get(`/energia-eolica`, (req, res) => {
-    let index = path.resolve('./view/html/pg1.html')
-    res.sendFile(index)
-})
-
+  let index = path.resolve("./view/html/pg1.html");
+  res.sendFile(index);
+});
 
 app.get(`/real-madrid`, (req, res) => {
-    let index = path.resolve('./view/html/pg2.html')
-    res.sendFile(index)
-})
+  let index = path.resolve("./view/html/pg2.html");
+  res.sendFile(index);
+});
 
 app.get(`/mudancas-leis-trabalhistas`, (req, res) => {
-    let index = path.resolve('./view/html/pg3.html')
-    res.sendFile(index)
-})
+  let index = path.resolve("./view/html/pg3.html");
+  res.sendFile(index);
+});
 
 app.get(`/cotas-de-faculdades`, (req, res) => {
-    let index = path.resolve('./view/html/pg4.html')
-    res.sendFile(index)
-})
+  let index = path.resolve("./view/html/pg4.html");
+  res.sendFile(index);
+});
 
-app.listen(3000, ()=> {
-    console.log(`Funfou`);
-})
+app.get(`/coreia-do-norte`, (req, res) => {
+  let index = path.resolve("./view/html/pg5.html");
+  res.sendFile(index);
+});
+
+app.listen(3000, () => {
+  console.log(`Funfou`);
+});
