890561814c83ff54827aca7c95a40dcecbd864ad
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-06-22 18:00:48 -0300

    Projeto de noticias com handlebars funcionando
    Projeto de noticias atualmente tem handlebars funcionando normalmente, porém ainda precisa de alguns ajustes adicionais

--- PW/Express/Jornal Project/controll.js
+++ PW/Express/Jornal Project/controll.js
@@ -11,34 +11,28 @@ router.use(
 
 router.use(express.json());
 
-app.get(`/`, (req, res) => {
-  let index = path.resolve("./view/index.html");
-  res.sendFile(index);
+router.get(`/`, (req, res) => {
+  res.render("index");
 });
 
-app.get(`/energia-eolica`, (req, res) => {
-  let index = path.resolve("./view/html/pg1.html");
-  res.sendFile(index);
+router.get(`/energia-eolica`, (req, res) => {
+  res.render("pg1");
 });
 
-app.get(`/real-madrid`, (req, res) => {
-  let index = path.resolve("./view/html/pg2.html");
-  res.sendFile(index);
+router.get(`/real-madrid`, (req, res) => {
+  res.render("pg2");
 });
 
-app.get(`/mudancas-leis-trabalhistas`, (req, res) => {
-  let index = path.resolve("./view/html/pg3.html");
-  res.sendFile(index);
+router.get(`/mudancas-leis-trabalhistas`, (req, res) => {
+  res.render("pg3");
 });
 
-app.get(`/cotas-de-faculdades`, (req, res) => {
-  let index = path.resolve("./view/html/pg4.html");
-  res.sendFile(index);
+router.get(`/cotas-de-faculdades`, (req, res) => {
+  res.render("pg4");
 });
 
-app.get(`/coreia-do-norte`, (req, res) => {
-  let index = path.resolve("./view/html/pg5.html");
-  res.sendFile(index);
+router.get(`/coreia-do-norte`, (req, res) => {
+  res.render("pg5");
 });
 
 export default router;
