ca9ca8dd65c6e5e834d5bb443f69fcab9189e710
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-08-07 14:42:34 -0300

    BD - dbRecursosHumanos updated
    The revision has been completed and the code is more pretty now.

--- BD/Views/dbRecursosHumanos/selects.sql
+++ BD/Views/dbRecursosHumanos/selects.sql
@@ -1,53 +1,82 @@
-USE bdRecursosHumanos
+USE bdRecursosHumanos;
 
 -- Exercício A
 CREATE VIEW Funcionarios_Departamento AS
-	SELECT tbDepartamento.nomeDepartamento, COUNT(codFuncionario) AS quantidade_funcionarios FROM tbDepartamento
-		INNER JOIN tbFuncionario ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
-
-	GROUP BY tbDepartamento.nomeDepartamento, tbFuncionario.codDepartamento
+SELECT 
+    tbDepartamento.nomeDepartamento, 
+    COUNT(tbFuncionario.codFuncionario) AS quantidade_funcionarios 
+FROM 
+    tbDepartamento
+INNER JOIN 
+    tbFuncionario ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
+GROUP BY 
+    tbDepartamento.nomeDepartamento;
 
 -- Exercício B
-SELECT nomeDepartamento FROM Funcionarios_Departamento 
-	WHERE quantidade_funcionarios = (SELECT MIN(quantidade_funcionarios) FROM Funcionarios_Departamento)
-	
+SELECT TOP 1 nomeDepartamento 
+	FROM Funcionarios_Departamento 
+		ORDER BY quantidade_funcionarios ASC;
+
 -- Exercício C
 CREATE VIEW Soma_Salarios AS
-	SELECT SUM(tbFuncionario.salarioFuncionario) AS soma_salarios FROM tbFuncionario
-		INNER JOIN tbDepartamento ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
-
-	GROUP BY tbDepartamento.nomeDepartamento, tbFuncionario.codDepartamento
+SELECT 
+    tbDepartamento.nomeDepartamento, 
+    SUM(tbFuncionario.salarioFuncionario) AS soma_salarios 
+FROM 
+    tbFuncionario
+INNER JOIN 
+    tbDepartamento ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
+GROUP BY 
+    tbDepartamento.nomeDepartamento;
 
 -- Exercício D
-SELECT soma_salarios FROM Soma_Salarios
-	WHERE soma_salarios = (SELECT max(soma_salarios) FROM Soma_Salarios)
+SELECT TOP 1 nomeDepartamento, soma_salarios 
+	FROM Soma_Salarios
+	ORDER BY soma_salarios DESC;
 
 -- Exercício E
 CREATE VIEW Funcionarios_sem_dependentes AS
-	SELECT tbFuncionario.nomeFuncionario as nome_funcionarios, tbDepartamento.nomeDepartamento nome_departamento FROM tbFuncionario
-		INNER JOIN tbDepartamento ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
-
-	WHERE tbFuncionario.codFuncionario NOT IN (SELECT codFuncionario FROM tbDependente)
+SELECT 
+    tbFuncionario.nomeFuncionario AS nome_funcionarios, 
+    tbDepartamento.nomeDepartamento AS nome_departamento 
+FROM 
+    tbFuncionario
+INNER JOIN 
+    tbDepartamento ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
+WHERE 
+    tbFuncionario.codFuncionario NOT IN (SELECT codFuncionario FROM tbDependente);
 
 -- Exercício F
 CREATE VIEW Dependentes_por_Funcionario AS 
-	SELECT nomeFuncionario, COUNT(codDependente) AS quantidade_dependentes FROM tbDependente
-		INNER JOIN tbFuncionario ON tbFuncionario.codFuncionario = tbDependente.codFuncionario
-
-	GROUP BY tbFuncionario.nomeFuncionario
+SELECT 
+    tbFuncionario.nomeFuncionario, 
+    COUNT(tbDependente.codDependente) AS quantidade_dependentes 
+FROM 
+    tbDependente
+INNER JOIN 
+    tbFuncionario ON tbFuncionario.codFuncionario = tbDependente.codFuncionario
+GROUP BY 
+    tbFuncionario.nomeFuncionario;
 
 -- Exercício G
-SELECT nomeFuncionario FROM Dependentes_por_Funcionario
-	WHERE quantidade_dependentes = (SELECT max(quantidade_dependentes) FROM Dependentes_por_Funcionario)
+SELECT TOP 1 nomeFuncionario 
+FROM Dependentes_por_Funcionario
+ORDER BY quantidade_dependentes DESC;
 
 -- Exercício H
 CREATE VIEW Quantidade_por_Departamento AS
-	SELECT nomeDepartamento, COUNT(tbDependente.codDependente) as quantidade_dependentes FROM tbDepartamento
-			INNER JOIN tbFuncionario ON  tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
-			INNER JOIN tbDependente ON tbFuncionario.codFuncionario = tbDependente.codFuncionario
-
-	GROUP BY tbDepartamento.nomeDepartamento
+SELECT 
+    tbDepartamento.nomeDepartamento, 
+    COUNT(tbDependente.codDependente) AS quantidade_dependentes 
+FROM 
+    tbDepartamento
+INNER JOIN 
+    tbFuncionario ON tbFuncionario.codDepartamento = tbDepartamento.codDepartamento
+INNER JOIN 
+    tbDependente ON tbFuncionario.codFuncionario = tbDependente.codFuncionario
+GROUP BY 
+    tbDepartamento.nomeDepartamento;
 
 -- Exercício I
-SELECT SUM(quantidade_dependentes) FROM Quantidade_por_Departamento
-.
+SELECT SUM(quantidade_dependentes) AS soma_geral_dependentes 
+FROM Quantidade_por_Departamento;
