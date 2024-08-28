58a4d74cf5d444c264fc84da3daebe9a89afec9c
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2024-08-28 10:35:04 -0300

    BD - Stored Procedure has been updated
    The stored procedure has been updated. dbEscola was used, not dbEscolaIdiomas.

--- BD/Stored Procedure/procedure.sql
+++ BD/Stored Procedure/procedure.sql
@@ -1,50 +1,34 @@
-﻿USE bdEscolaDeIdiomas
+﻿USE dbEscola
 
-
-
--- Exerc�cio 1: Criar uma stored procedure �Busca_Aluno� que receba o c�digo do aluno e retorne seu nome e data de nascimento.
+-- Exercício 1: Criar uma stored procedure “Busca_Aluno” que receba o código do aluno e retorne seu nome e data de nascimento.
 CREATE PROCEDURE Busca_Aluno
 @codigoAluno INT
 AS
-BEGIN
 	SELECT nomeAluno, dataNascAluno	FROM tbAluno
 		WHERE codAluno = @codigoAluno
-END
 
 EXEC Busca_Aluno 1
 
--- Exerc�cio 2: Criar uma stored procedure �Insere_Aluno� que insira um registro na tabela de Alunos.
-CREATE PROCEDURE Insere_Aluno  
-    @nomeAluno VARCHAR(70),
-    @rgAluno VARCHAR(12),
-    @cpfAluno VARCHAR(15),
-    @logradouro VARCHAR(70),
-    @numero VARCHAR(20),
-    @complemento VARCHAR(50) = NULL,
-    @cep VARCHAR(9),
-    @bairro VARCHAR(50),
-    @cidade VARCHAR(50),
-    @dataNascAluno SMALLDATETIME
+-- Exercício 2: Criar uma stored procedure “Insere_Aluno” que insira um registro na tabela de Alunos.
+CREATE PROCEDURE Insere_Aluno 
+@nomeAluno VARCHAR(255),
+@dataNascAluno DATE,
+@rgAluno VARCHAR(50),
+@naturalidadeAluno VARCHAR(2)
 AS
-BEGIN
-	INSERT INTO tbAluno(nomeAluno, rgAluno, cpfAluno, logradouro, numero, complemento, cep, bairro, cidade, dataNascAluno)
-		VALUES (
-        @nomeAluno, @rgAluno, @cpfAluno, @logradouro, @numero, @complemento, @cep, @bairro, @cidade, @dataNascAluno
-		);
-END
+	INSERT INTO tbAluno(nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno) VALUES
+	(@nomeAluno, @dataNascAluno, @rgAluno, @naturalidadeAluno)
 
-EXEC Insere_Aluno 'Patrick Lessa Teixeira', '123456789', '12345678900', 'Rua Faultline', '10A', 'casa', '08121210', 'Silverchair Paulista', 'São Paulo', '03/05/2002'
+EXEC Insere_Aluno 'Paulo Santos', '2000-03-10 00:00:00', '82.282.122-0', 'SP'
 
--- Exerc�cio 3:  Criar uma stored procedure �Aumenta_Preco� que, dados o nome do curso e um percentual, aumente o valor do curso com a porcentagem informada
+-- Exercício 3:  Criar uma stored procedure “Aumenta_Preco” que, dados o nome do curso e um percentual, aumente o valor do curso com a porcentagem informada
 CREATE PROCEDURE Aumenta_Preco
 @nomeCurso VARCHAR(255),
 @percentualAumento FLOAT
 AS
-BEGIN
 	UPDATE tbCurso
 		SET valorCurso = valorCurso + (valorCurso / 100 * @percentualAumento)
 	WHERE nomeCurso = @nomeCurso
-END
 
 EXEC Aumenta_Preco 'Inglês', 10
 
@@ -54,10 +38,10 @@ CREATE PROCEDURE Exibe_Turma
 AS
 BEGIN
 	SELECT * FROM tbTurma
-		WHERE descTurma = @nomeTurma
+		WHERE nomeTurma = @nomeTurma
 END
 
-EXEC Exibe_Turma 'Inglês 1A'
+EXEC Exibe_Turma '1|A'
 
 -- Exercício 5: Criar uma stored procedure “Exibe_AlunosdaTurma” que, dado o nome da turma exiba os seus alunos.
 CREATE PROCEDURE Exibe_AlunosdaTurma
@@ -71,45 +55,37 @@ SELECT nomeAluno FROM tbAluno
         INNER JOIN tbTurma
             ON tbMatricula.codTurma = tbTurma.codTurma
 
-     WHERE tbTurma.descTurma = @nomeTurma
+     WHERE tbTurma.nomeTurma = @nomeTurma
 END
 
-EXEC Exibe_AlunosdaTurma 'Inglês 1A'
+EXEC Exibe_AlunosdaTurma '1|A'
 
 -- Exercício 6: Criar uma stored procedure para inserir alunos, verificando pelo cpf se o aluno existe ou não, e informar essa condição via mensagem
 CREATE PROCEDURE spInsereAlunos  
-    @nomeAluno VARCHAR(70),
-    @rgAluno VARCHAR(12),
-    @cpfAluno VARCHAR(15),
-    @logradouro VARCHAR(70),
-    @numero VARCHAR(20),
-    @complemento VARCHAR(50) = NULL,
-    @cep VARCHAR(9),
-    @bairro VARCHAR(50),
-    @cidade VARCHAR(50),
-    @dataNascAluno SMALLDATETIME
+@nomeAluno VARCHAR(255),
+@dataNascAluno DATE,
+@rgAluno VARCHAR(50),
+@naturalidadeAluno VARCHAR(2)
 AS
 BEGIN 
 	DECLARE @idAluno INT
-	IF exists(SELECT cpfAluno FROM tbAluno WHERE cpfAluno = @cpfAluno)
+	IF exists(SELECT rgAluno FROM tbAluno WHERE rgAluno = @rgAluno)
 	BEGIN
 		print('Não foi possível completar o cadastro. Já existe um aluno cadastrado no sistema usando esse CPF.')
 	END
 
 	ELSE
 	BEGIN
+		INSERT INTO tbAluno(nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno) VALUES
+			(@nomeAluno, @dataNascAluno, @rgAluno, @naturalidadeAluno)
 
-		INSERT INTO tbAluno(nomeAluno, rgAluno, cpfAluno, logradouro, numero, complemento, cep, bairro, cidade, dataNascAluno)
-			VALUES (
-				@nomeAluno, @rgAluno, @cpfAluno, @logradouro, @numero, @complemento, @cep, @bairro, @cidade, @dataNascAluno
-			);
 		SET @idAluno = (select MAX(codAluno) FROM tbAluno)
 		PRINT('Aluno ' + @nomeAluno + ' foi cadastrado com sucesso, código ' + convert(VARCHAR(5), @idAluno))
 
 	END
 END
 
-EXEC spInsereAlunos 'Patrick Lessa Teixeira', '123456789', '12345678900', 'Rua Faultline', '10A', 'casa', '08121210', 'Silverchair Paulista', 'São Paulo', '03/05/2002'
+EXEC spInsereAlunos'Paulo Santos', '2000-03-10 00:00:00', '82.282.122-0', 'SP'
 
 -- Exercício 7:  Criar uma stored procedure que receba o nome do curso e o nome do aluno e matricule o mesmo no curso pretendido
 CREATE PROCEDURE spMatriculaAluno 
@@ -120,7 +96,7 @@ BEGIN
 	DECLARE @codAluno INT
 	DECLARE @codTurma INT
 
-	SET @codAluno = (SELECT codAluno FROM tbAluno WHERE nomeAluno = @nomeAluno)
+	SET @codAluno = (SELECT TOP 1 codAluno FROM tbAluno WHERE nomeAluno = @nomeAluno)
 	SET @codTurma = (SELECT TOP 1 codTurma FROM tbTurma 
 		INNER JOIN tbCurso ON tbTurma.codCurso = tbCurso.codCurso
 		WHERE tbCurso.nomeCurso = @nomeCurso)
@@ -136,7 +112,6 @@ BEGIN
 	END
 END
 
-EXEC spMatriculaAluno 'Inglês', 'Patrick Lessa Teixeira'
+EXEC spMatriculaAluno 'Inglês', 'Paulo Santos'
 
 -- OBS: Devido a ter a possibilidade de existir mais de uma turma que faz determinado tipo de curso (como duas turmas que fazem curso de inglês) eu fiz com que o SQL pegue apenas a primeira turma que aparecer
-
