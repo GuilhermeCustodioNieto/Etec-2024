318579afa8e48200f1aaf0515e78b0bdfaead768
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-10-16 00:22:33 -0300

    PAM - Básico do site de formas geométricas finalizado

--- PAM/JavaScript/Formas Geométricas Project/script.js
+++ PAM/JavaScript/Formas Geométricas Project/script.js
@@ -1,66 +1,71 @@
-var lados = prompt("Insira a quantidade de Lados:");
-
-function Formas(lados) {
-  const novaDiv = document.createElement("div");
+function verificaForma(lados) {
   if (lados == 3) {
-    var resultado =
-      "A forma geometrica é o Triangulo" +
-      " Formula do Perimetro: p = l+l+l" +
-      " Formula da Área: A = b*h/2";
-    novaDiv.appendChild(conteudo);
-    var currentDiv = document.getElementById("div1");
-    document.body.insertBefore(novaDiv, currentDiv);
+    return (
+      "A forma geometrica é o Triangulo\n" +
+      " Formula do Perimetro: p = l+l+l\n" +
+      " Formula da Área: A = b*h/2"
+    );
   } else if (lados == 4) {
-    console.log(
-      "A forma geometrica é o Quadrado" +
-        " Formula do Perimetro: p = b*h" +
-        " Formula da Área: A = b*b"
+    return (
+      "A forma geometrica é o Quadrado\n" +
+      " Formula do Perimetro: p = b*h\n" +
+      " Formula da Área: A = b*b"
     );
   } else if (lados == 5) {
-    console.log(
-      "A forma geometrica é o Pentágono" +
-        " Formula do Perimetro: p = a*5" +
-        " Formula da Área: A =1/4 raiz de 5*5+2*raiz de 5 * a*2"
+    return (
+      "A forma geometrica é o Pentágono\n" +
+      " Formula do Perimetro: p = a*5\n" +
+      " Formula da Área: A = 1/4 raiz de 5*5+2*raiz de 5 * a*2"
     );
   } else if (lados == 6) {
-    console.log(
-      "A forma geometrica é o Hexágono" +
-        " Formula do Perimetro: p = a*6" +
-        " Formula da Área: A =3*1,2/2*a*2"
+    return (
+      "A forma geometrica é o Hexágono\n" +
+      " Formula do Perimetro: p = a*6\n" +
+      " Formula da Área: A = 3*1,2/2*a*2"
     );
   } else if (lados == 7) {
-    console.log(
-      "A forma geometrica é o Heptágono" +
-        " Formula do Perimetro: p = a*7" +
-        " Formula da Área: A =7/4a*2cot* pi/7"
+    return (
+      "A forma geometrica é o Heptágono\n" +
+      " Formula do Perimetro: p = a*7\n" +
+      " Formula da Área: A = 7/4a*2cot* pi/7"
     );
   } else if (lados == 8) {
-    console.log(
-      "A forma geometrica é o Octógono" +
-        " Formula do Perimetro: p = a*8" +
-        " Formula da Área: A =2*1+raiz de 2*a*2"
+    return (
+      "A forma geometrica é o Octógono\n" +
+      " Formula do Perimetro: p = a*8\n" +
+      " Formula da Área: A = 2*1+raiz de 2*a*2"
     );
   } else if (lados == 9) {
-    console.log(
-      "A forma geometrica é o nonaágono" +
-        " Formula do Perimetro: p = a*9" +
-        " Formula da Área: A =9/4a>2cot* pi/9"
+    return (
+      "A forma geometrica é o Nonaágono\n" +
+      " Formula do Perimetro: p = a*9\n" +
+      " Formula da Área: A = 9/4a*2cot* pi/9"
     );
   } else if (lados == 10) {
-    console.log(
-      "A forma geometrica é o Decágono" +
-        " Formula do Perimetro: p = a*10" +
-        " Formula da Área: A = 5/2a>2cot* pi/10"
+    return (
+      "A forma geometrica é o Decágono\n" +
+      " Formula do Perimetro: p = a*10\n" +
+      " Formula da Área: A = 5/2a*2cot* pi/10"
     );
+  } else {
+    return "O número de lados não se enquadra nos números aceitos\nApenas de 3 a 10";
   }
 }
 
-function criarElemento(resultado) {
-  var div = document.createElement("div");
-  var counteudo = document.createTextNode(resultado);
-  div.appendChild(conteudo);
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(novaDiv, currentDiv);
+function mostraLados(lados) {
+  console.log("chamou");
+
+  let divForma = document.querySelector("#dados");
+
+  const dados = verificaForma(lados);
+
+  divForma.innerHTML += dados.replace(/\n/g, "<br>");
 }
 
-console.log(Formas(lados));
+const enviarDadosBtn = document
+  .querySelector("#dados-form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const lados = document.querySelector("#lados").value;
+    mostraLados(lados);
+  });
